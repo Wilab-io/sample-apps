57361ed3662771871f8fa300b1bd5d69e9eaf83a
Agustín Clemente <aclemente@wilab.io>
2024-11-28 12:31:55 -0300

    fix(docker): don't run shad4fast at startup

--- visual-retrieval-colpali/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd src
-shad4fast build
-if [ ! -f output.css ]; then
-    echo "Error: output.css was not generated!"
-    exit 1
-fi
-echo "output.css was generated successfully"
-python main.py
